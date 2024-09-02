8a79bcb515a482ea0957cb6d8605ff58526117e4
ErvinApk I-2-21 <66515174+Erwin503@users.noreply.github.com>
2024-09-02 14:25:18 +0300

    Update main3_4.py

--- main3_4.py
+++ main3_4.py
@@ -1,8 +1,12 @@
 def single_root_words(root_word, *other_words):
     same_words = []
+    root_word_lower = root_word.lower()
+    
     for word in other_words:
-        if root_word in word or word in root_word:
+        word_lower = word.lower()
+        if root_word_lower in word_lower or word_lower in root_word_lower:
             same_words.append(word)
+    
     return same_words
 
 result = single_root_words("root", "rooted", "uproot", "tree", "rooting", "roots", "grass")
