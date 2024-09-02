8643f36e138fbd916deeda473b0da0c558c79c95
ErvinApk I-2-21 <66515174+Erwin503@users.noreply.github.com>
2024-09-02 14:19:35 +0300

    Update main2_2.py

--- main2_2.py
+++ main2_2.py
@@ -1,5 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-res = [i  for i in my_list if i > 0]
+res = []
+i = 0
+
+while i < len(my_list):
+    if my_list[i] > 0:
+        res.append(my_list[i])
+    i += 1
 
 print(res)
