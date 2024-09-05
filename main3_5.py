5903a4a8356c0f37d26d2b43db0cd3641801aaa3
ErvinApk I-2-21 <66515174+Erwin503@users.noreply.github.com>
2024-09-05 15:38:47 +0300

    3 hard

--- main3_5.py
+++ main3_5.py
@@ -1,12 +1,30 @@
-def get_multiplied_digits(number):
-    str_number = str(number)
+def calculate_structure_sum(data_structure):
+    total_sum = 0
+    def recursive_sum(element):
+        nonlocal total_sum
+        
+        if isinstance(element, int):
+            total_sum += element
+        elif isinstance(element, str):
+            total_sum += len(element)
+        elif isinstance(element, (list, tuple, set)):
+            for item in element:
+                recursive_sum(item)
+        elif isinstance(element, dict):
+            for key, value in element.items():
+                recursive_sum(key)
+                recursive_sum(value)
     
-    first = int(str_number[0])
-    
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+    recursive_sum(data_structure)
+    return total_sum
 
-result = get_multiplied_digits(40203)
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
 print(result)
