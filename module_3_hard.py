8e2697484edb9226fccf73c5a5ff5e49fa069937
ErvinApk I-2-21 <66515174+Erwin503@users.noreply.github.com>
2024-09-05 16:09:59 +0300

    4_2

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,30 +1,15 @@
-def calculate_structure_sum(data_structure):
-    total_sum = 0
-    def recursive_sum(element):
-        nonlocal total_sum
-        
-        if isinstance(element, int):
-            total_sum += element
-        elif isinstance(element, str):
-            total_sum += len(element)
-        elif isinstance(element, (list, tuple, set)):
-            for item in element:
-                recursive_sum(item)
-        elif isinstance(element, dict):
-            for key, value in element.items():
-                recursive_sum(key)
-                recursive_sum(value)
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
     
-    recursive_sum(data_structure)
-    return total_sum
+    # Вызов inner_function внутри test_function
+    inner_function()
 
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+# Вызов test_function
+test_function()
 
-result = calculate_structure_sum(data_structure)
-print(result)
+# Попытка вызвать inner_function вне test_function
+try:
+    inner_function()  # Этот вызов приведет к ошибке
+except NameError as e:
+    print(f"Ошибка: {e}")
